0a4014ddc02740dee42c1dca78f6c49d8aa08f18
Ryan Charles Smith <ryan-smith@comcast.net>
2019-10-22 20:15:36 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,14 +1,16 @@
 array_one = [1,2,3]
 
+
 def square_array(array)
   i = 0
+  new_array= [ ]
   while i < array.length do
-  new = square_array[i] * square_array[i]
-  new_array.push[new]
-  p new_array
-  i+=
-  end
+      new = square_array[i] * square_array[i]
+    new_array.push[new]
   
+    i+=
+  end
+  p new_array
 end
 
 square_array(array_one)
